0e5b9c4eb754a3e0921d70781b1feca3c9fed7ce
Patrick Aguilera <aguilerapatrick66@gmail.com>
2026-03-24 02:58:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ window.onload = () => {
     // BOTONES DE NAVEGACIÓN
     if(get("playBtn")) get("playBtn").onclick = startGame;
     if(get("restartBtn")) get("restartBtn").onclick = startGame;
+    if(get("rankingBtn")) get("rankingBtn").onclick = showRanking;
 };
 
 async function loadWallet() {
