269e80fd324edd51881ac73122ac04f6a3f15cc8
Patrick Aguilera <aguilerapatrick66@gmail.com>
2026-03-25 17:23:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const get=id=>document.getElementById(id);
 
-let score=0,time=15;
-let combo=1;
-let comboTimer;
-
-let gameTimer,spawnTimer;
+let score=0,time=15,combo=1;
+let spawnRate=700;
+let gameTimer,spawnTimer,comboTimer;
 
 window.onload=()=>{
 get("playBtn").onclick=startGame;
@@ -20,6 +18,7 @@ function startGame(){
 score=0;
 time=15;
 combo=1;
+spawnRate=700;
 
 switchScreen("game");
 updateUI();
@@ -30,42 +29,62 @@ updateUI();
 if(time<=0)endGame();
 },1000);
 
-spawnTimer=setInterval(spawnSkull,700);
+spawnTimer=setInterval(spawnEnemy,spawnRate);
 }
 
-function spawnSkull(){
+function spawnEnemy(){
+const isBoss=Math.random()<0.1;
+
 const skull=document.createElement("div");
 skull.className="target";
+if(isBoss) skull.classList.add("boss");
+
 skull.innerHTML="💀";
 
-/* 🌈 COLOR NEON RANDOM */
-const colors=["#00fff7","#ff00ff","#00ff00","#ffcc00"];
+const colors=["#0ff","#f0f","#0f0","#ff0"];
 const color=colors[Math.floor(Math.random()*colors.length)];
-skull.style.textShadow=`0 0 10px ${color},0 0 20px ${color}`;
 
-skull.style.left=Math.random()*90+"%";
-skull.style.top=Math.random()*90+"%";
+skull.style.textShadow=`0 0 15px ${color}`;
+
+skull.style.left=Math.random()*85+"%";
+skull.style.top=Math.random()*85+"%";
 
 skull.onclick=(e)=>{
+
 hitEffect(e.clientX,e.clientY,color);
 
-score+=10*combo;
+/* puntos */
+score+=isBoss ? 50*combo : 10*combo;
 
-/* 🔥 COMBO REAL */
+/* combo */
 combo++;
 clearTimeout(comboTimer);
 comboTimer=setTimeout(()=>combo=1,1500);
+
+/* sonido */
+let sound = combo > 5 ? get("hit2") : get("hit1");
+sound.currentTime=0;
+sound.play();
+
+/* vibracion */
+get("gameArea").classList.add("shake");
+setTimeout(()=>get("gameArea").classList.remove("shake"),200);
+
+/* dificultad progresiva */
+if(get("survivalMode").checked){
+spawnRate*=0.98;
+clearInterval(spawnTimer);
+spawnTimer=setInterval(spawnEnemy,spawnRate);
+}
 
 updateUI();
 skull.remove();
 };
 
 get("gameArea").appendChild(skull);
-
 setTimeout(()=>skull.remove(),1200);
 }
 
-/* 💥 EFECTO EXPLOSION + PARTICULAS */
 function hitEffect(x,y,color){
 
 const explosion=document.createElement("div");
@@ -75,19 +94,19 @@ explosion.style.top=y+"px";
 explosion.style.background=color;
 
 document.body.appendChild(explosion);
-
 setTimeout(()=>explosion.remove(),400);
 
-/* partículas */
-for(let i=0;i<8;i++){
+/* partículas optimizadas */
+for(let i=0;i<5;i++){
 const p=document.createElement("div");
 p.className="particle";
+
 p.style.left=x+"px";
 p.style.top=y+"px";
 p.style.background=color;
 
-p.style.setProperty("--x",(Math.random()*100-50)+"px");
-p.style.setProperty("--y",(Math.random()*100-50)+"px");
+p.style.setProperty("--x",(Math.random()*80-40)+"px");
+p.style.setProperty("--y",(Math.random()*80-40)+"px");
 
 document.body.appendChild(p);
 setTimeout(()=>p.remove(),600);
